b5075e0099e42c1f306c52e5243701b5d0459347
Ondrej Stranik <101558639+ondrejstranik@users.noreply.github.com>
2025-07-01 11:42:52 +0200

    working coding decoding  BDdata

--- tests/testingScript2.py
+++ tests/testingScript2.py
@@ -21,12 +21,12 @@ class BHData():
         nStream = len(stream)
 
         self.macroTime= (stream&0x0FFF).astype('uint16')
-        self.channel = (stream&0xF000 >> 12).astype('uint8')
-        self.microTime = (stream&0x0FFF_0000 >> 16).astype('uint16')
-        self.noPhotonFlag = (stream&0b1000_0000_0000_0000 >>31).astype('bool')
-        self.newLineFlag = (stream&0b0001_0000_0000_0000 >> 28).astype('bool')
-        self.stackOverflowFlag = (stream&0b0010_0000_0000_0000 >> 29).astype('bool')
-        self.newMacroTimeFlag = (stream&0b0100_0000_0000_0000 >> 30).astype('bool')
+        self.channel = (stream>> 12 & 0xF).astype('uint8')
+        self.microTime = (stream>> 16 &0x0FFF).astype('uint16')
+        self.noPhotonFlag = (stream>>31&0b1).astype('bool')
+        self.newLineFlag = (stream>> 28&0b1).astype('bool')
+        self.stackOverflowFlag = (stream >> 29 &0b1).astype('bool')
+        self.newMacroTimeFlag = (stream>> 30 &0b1).astype('bool')
         self.nData = len(self.channel)
 
     def dataToStream(self):
@@ -34,13 +34,13 @@ class BHData():
         return int32 numpy 1D array 
         '''
         stream= np.empty(self.nData, dtype='uint32')
-        stream = ((self.noPhotonFlag<<31).astype('uint32') 
-                + (self.newMacroTimeFlag<<30).astype('uint32') 
-                + (self.stackOverflowFlag<<29).astype('uint32')
-                + (self.newLineFlag <<28).astype('uint32')
-                + (self.microTime<<16).astype('uint32')
-                + (self.channel<<12).astype('uint32') 
-                + (self.macroTime).astype('uint32'))
+        stream = ((self.noPhotonFlag.astype('uint32')<<31) 
+                + (self.newMacroTimeFlag.astype('uint32')<<30) 
+                + (self.stackOverflowFlag.astype('uint32')<<29)
+                + (self.newLineFlag.astype('uint32') <<28)
+                + (self.microTime.astype('uint32')<<16)
+                + (self.channel.astype('uint32')<<12) 
+                + self.macroTime.astype('uint32'))
         return stream
 
     def _setArbitraryData(self,length=100):
@@ -51,9 +51,9 @@ class BHData():
         self.stackOverflowFlag = np.random.randint(2,size= self.nData,dtype='bool')
         self.noPhotonFlag = (self.newLineFlag | self.newMacroTimeFlag 
                              | self.stackOverflowFlag)
-        self.macroTime= np.random.randint(2**12, size=self.nData)
-        self.microTime= np.random.randint(2**12, size=self.nData)
-        self.channel = np.random.randint(2**4, size=self.nData)
+        self.macroTime= np.random.randint(2**12, size=self.nData, dtype='uint16')
+        self.microTime= np.random.randint(2**12, size=self.nData, dtype='uint16')
+        self.channel = np.random.randint(2**4, size=self.nData, dtype='uint8')
 
 #%%
 
@@ -65,8 +65,8 @@ myBHData2 = BHData()
 myBHData2.streamToData(myStream)
 
 print(f'stream \n {myStream}')
-print(myBHData.microTime)
-print(myBHData2.microTime)
+print(f'microtime original \n {myBHData.microTime}')
+print(f'microtime recovered \n {myBHData2.microTime}')
 
 print(np.all(myBHData.microTime==myBHData2.microTime))
 
